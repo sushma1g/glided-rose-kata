2bca0ea09a7617d366040865d28a1c95d93c1af8
sushma1g <119601014+sushma1g@users.noreply.github.com>
2022-12-01 09:56:19 -0500

    Update gilded-rose.ts

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -20,9 +20,9 @@ export class GildedRose {
   updateQuality() {
     for (let i = 0; i < this.items.length; i++) {
       if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-        if (this.items[i].quality > 0) {
+        if (this.items[i].quality > 0 ) {
           if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-            if (this.items[i].name != 'Conjured') {
+            if (this.items[i].name !== 'Conjured') {
               this.items[i].quality = this.items[i].quality - 1
             }else{
               if (this.items[i].quality >= 2){
@@ -62,7 +62,7 @@ export class GildedRose {
           if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
             if (this.items[i].quality > 0) {
               if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                if (this.items[i].name != 'Conjured') {
+                if (this.items[i].name !== 'Conjured') {
                   this.items[i].quality = this.items[i].quality - 1
                   }else{
                     if( this.items[i].quality > 1){
@@ -84,7 +84,16 @@ export class GildedRose {
           }
         }
       }
+
+      if(this.items[i].quality > 50 && this.items[i].name !== 'Sulfuras, Hand of Ragnaros'){
+        this.items[i].quality = 50
+      }
+
+      if(this.items[i].quality < 0){
+        this.items[i].quality =0;
+      }
     }
+
 
     return this.items;
   }
